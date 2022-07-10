af871472902221eae7ea6b767dd0de18b92067c5
Estherfisayomi12 <fisayomi2602@gmail.com>
2022-07-10 22:06:55 +0100

    Updated the form validation

--- script.js
+++ script.js
@@ -1,221 +1,102 @@
 const url = "https://api.cloudinary.com/v1_1/Estherfisayomi12/auto/upload";
 const form = document.querySelector("#fileUpload");
-// console.log(form);
+const emailField = document.querySelector("#email");
+const coverLetterField = document.querySelector("#cover-letter");
+const allegianceField = document.querySelector("#allegiance");
+const signupForm = document.querySelector("#signup");
+
+const fileValidation = (file) => {
+  const isValidFile =
+    file.type === "image/jpeg" ||
+    file.type === "image/png" ||
+    file.type === "application/msword" ||
+    file.type ===
+      "application/vnd.openxmlformats-officedocument.wordprocessingml.document" ||
+    file.type === "application/pdf";
+
+  if (!isValidFile) {
+    document.querySelector("#file-error").innerHTML =
+      "You can only upload JPG/PNG/Docx/PDF file!";
+    return false;
+  }
+
+  const isValidSize = file.size / 1024 / 1024 < 1;
+
+  if (!isValidSize) {
+    document.querySelector("#file-error").innerHTML = "File limit is 1MB!";
+    return false;
+  }
+
+  return isValidFile && isValidSize;
+};
+
+const formValidation = () => {
+  if (emailField.value === "") {
+    document.querySelector("#email-error").innerHTML = "Email is required";
+    return false;
+  } else {
+    document.querySelector("#email-error").innerHTML = "";
+  }
+
+  if (coverLetterField.value === "") {
+    document.querySelector("#cover-error").innerHTML =
+      "Cover letter is required";
+    console.log(form.files);
+    return false;
+  } else {
+    document.querySelector("#cover-error").innerHTML = "";
+  }
+
+  if (form.files.length === 0) {
+    document.querySelector("#file-error").innerHTML = "File Upload is required";
+    return false;
+  } else {
+    document.querySelector("#file-error").innerHTML = "";
+  }
+
+  if (allegianceField.checked === false) {
+    document.querySelector("#allegiance-error").innerHTML =
+      "This field is required";
+    return false;
+  }
+};
 
 form.onchange = async (e) => {
   const files = e.target.files;
-  console.log(files);
 
   const formData = new FormData();
 
   for (let i = 0; i < files.length; i++) {
-    let file = files[i];
-    formData.append("file", file);
-    formData.append("upload_preset", "bc04kr0f");
+    const isValidFile =
+      files[0].type === "image/jpeg" ||
+      files[0].type === "image/png" ||
+      files[0].type === "application/msword" ||
+      files[0].type ===
+        "application/vnd.openxmlformats-officedocument.wordprocessingml.document" ||
+      files[0].type === "application/pdf";
 
-    const response = await axios.post(url, formData, {
-      headers: { "Content-Type": "multipart/form-data" },
-    });
-    document.getElementById("fileUrl").setAttribute("value", response.data.url);
-  }
-};
+    const isValidSize = files[0].size / 1024 / 1024 < 1;
 
-/*const url = "https://api.cloudinary.com/v1_1/Estherfisayomi12/auto/upload";
-const form = document.querySelector("#signup");
+    if (!isValidFile) {
+      form.value = "";
+      document.querySelector("#file-error").innerHTML =
+        "You can only upload JPG/PNG/Docx/PDF file!";
+      return false;
+    } else if (!isValidSize) {
+      form.value = "";
+      document.querySelector("#file-error").innerHTML = "File limit is 1MB!";
+      return false;
+    } else {
+      let file = files[i];
+      formData.append("file", file);
+      formData.append("upload_preset", "bc04kr0f");
 
-form.addEventListener("submit", async (e) => {
-  e.preventDefault();
-
-  const files = document.querySelector("[type=file]").files;
-  const formData = new FormData();
-
-  for (let i = 0; i < files.length; i++) {
-    let file = files[i];
-    formData.append("file", file);
-    // Add your Upload preset here
-    formData.append("upload_preset", "bc04kr0f");
-
-    const response = await axios.post(url, formData, {
-      headers: { "Content-Type": "multipart/form-data" },
-    });
-
-    console.log(response.data.url);
-
-    document
-      .getElementById("viewSubmission")
-      .setAttribute("href", response.data.url);
-  }
-});
-
-//FORM VALIDATION
-
-const emailEl = document.querySelector("#email");
-const coverLetterEl = document.querySelector("#cover-letter");
-const allegianceEl = document.querySelector("#allegiance");
-
-const Form = document.querySelector("form");
-
-const checkEmail = () => {
-  let valid = false;
-  const min = 3,
-    max = 25;
-
-  const email = emailEl.value.trim();
-
-  if (!isRequired(email)) {
-    showError(emailEl, "Email cannot be blank");
-  } else if (!isbetween(email.length, min, max)) {
-    showError(emailEl, `Email must be between ${min} and ${max} characters.`);
-  } else if (!isEmaiLValid(email)) {
-    showError(emailEl, "Email is not valid.");
-  } else {
-    showSuccess(emailEl);
-    valid = true;
-  }
-
-  return valid;
-};
-
-const checkCoverLetter = () => {
-  let valid = false;
-  const min = 100,
-    max = 250;
-
-  const coverLetter = coverLetterEl.value.trim();
-
-  if (!isRequired(coverLetter)) {
-    showError(coverLetterEl, "Cover Letter cannot be blank");
-  } else if (!isbetween(coverLetter.length, min, max)) {
-    showError(
-      coverLetterEl,
-      `Cover Letter must be between ${min} and ${max} characters.`
-    );
-  } else {
-    showSuccess(coverLetterEl);
-    valid = true;
-  }
-
-  return valid;
-};
-
-function fileValidation() {
-  const fileInput = document.getElementById("file");
-
-  const filePath = fileInput.value;
-
-  // Allowing file type
-  const allowedExtensions =
-    /(\.doc|\.docx|\.odt|\.pdf|\.tex|\.txt|\.rtf|\.wps|\.wks|\.wpd)$/i;
-
-  if (!allowedExtensions.exec(filePath)) {
-    document.querySelector(".file-upload").classList.add("error");
-    document.querySelector(".file-upload").textContent =
-      "Acceptable file types: pdf, doc, docx";
-    fileInput.value = "";
-    return false;
-  } else {
-    document.querySelector(".file-upload").classList.remove("error");
-    document.querySelector(".file-upload").textContent = fileInput.value;
-  }
-}
-
-// const checkAllegiance = () => {
-//   let valid = false;
-
-//   const allegiance = allegianceEl.value.trim();
-
-//   if (isRequired(allegiance)) {
-//     showError(allegianceEl), "Please click the button";
-//   } else {
-//     showSuccess(allegianceEl);
-//     valid = true;
-//   }
-
-//   return valid;
-// };
-
-const isEmaiLValid = (email) => {
-  const re =
-    /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
-
-  return re.test(email);
-};
-
-const isRequired = (value) => (value === "" ? false : true);
-const isbetween = (length, min, max) =>
-  length < min || length > max ? false : true;
-
-const showError = (input, message) => {
-  //get the form-field element
-  const formField = input.parentElement;
-  // add the error class
-  formField.classList.remove("success");
-  formField.classList.add("error");
-
-  //show the error message
-  const error = formField.querySelector("small");
-  error.textContent = message;
-};
-
-const showSuccess = (input) => {
-  //get the form-field element
-  const formField = input.parentElement;
-
-  //remove the error class
-  formField.classList.remove("error");
-  formField.classList.add("success");
-
-  //hide the error message
-  const error = formField.querySelector("small");
-  error.textContent = "";
-};
-
-Form.addEventListener("submit", function (e) {
-  //prevent the form from submitting
-
-  e.preventDefault();
-
-  //validate fields
-  let isEmailValid = checkEmail(),
-    isCoverLetterValid = checkCoverLetter();
-  //isAllegianceValid = checkAllegiance();
-
-  let isFormValid = isEmaiLValid && isCoverLetterValid;
-  //&& isAllegianceValid;
-
-  // submit to the server if the form is valid
-  if (isFormValid) {
-  }
-});
-
-const debounce = (fn, delay = 500) => {
-  let timeoutId;
-  return (...args) => {
-    // cancel the previous timer
-    if (timeoutId) {
-      clearTimeout(timeoutId);
+      const response = await axios.post(url, formData, {
+        headers: { "Content-Type": "multipart/form-data" },
+      });
+      document
+        .getElementById("fileUrl")
+        .setAttribute("value", response.data.url);
     }
-    // setup a new timer
-    timeoutId = setTimeout(() => {
-      fn.apply(null, args);
-    }, delay);
-  };
+  }
 };
-
-form.addEventListener(
-  "input",
-  debounce(function (e) {
-    switch (e.target.id) {
-      case "email":
-        checkEmail();
-        break;
-      case "coverLetter":
-        checkCoverLetter();
-        break;
-      //case "allegiance":
-      //checkAllegiance();
-      //break;
-    }
-  })
-);
-*/
